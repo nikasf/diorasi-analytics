c10c1ff0db525ce207e43533fe4cef3218271211
Sofia Nika <nika.sofia.ns@gmail.com>
2022-08-07 14:50:01 +0300

    Initial commit

---
+++ src/main/java/gr/snika/diorasi/DiorasiApplication.java
@@ -0,0 +1,13 @@
+package gr.snika.diorasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiorasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiorasiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/snika/diorasi/DiorasiApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.snika.diorasi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiorasiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
